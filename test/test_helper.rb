933acbe2776b40670ad1b4281daedd37d3c15172
Jonathan Garvin <jon@5valleys.com>
2014-03-13 20:45:29 -0600

    add stdout capturing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,35 @@ require 'minionizer'
 module Minionizer
   class MiniTest::Unit::TestCase
 
+    def before_setup
+      super
+      capture_stdout
+    end
+
+    def after_teardown
+      super
+      release_stdout
+    end
+
+    #######
+    private
+    #######
+
+    def capture_stdout
+      @stdout = $stdout
+      $stdout = StringIO.new
+    end
+
+    def release_stdout
+      $stdout = @stdout
+    end
+
+    # Redirect intentional puts from within tests
+    # to the *real* STDOUT for troubleshooting.
+    def puts(*args)
+      @stdout.puts(*args)
+    end
+
   end
 end
 
